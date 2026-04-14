2f4e37278c73bb971b508b6b983ee5a345964221
Ary2506 <ap1895284@gmail.com>
2026-04-14 17:01:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
 //your code here!
+const list = document.getElementById("infi-list");
 
+let count = 0;
+
+function addItems(num) {
+  for (let i = 0; i < num; i++) {
+    const li = document.createElement("li");
+    li.textContent = "Item " + (++count);
+    list.appendChild(li);
+  }
+}
+
+addItems(10);
+
+list.addEventListener("scroll", () => {
+  if (list.scrollTop + list.clientHeight >= list.scrollHeight) {
+    addItems(2);
+  }
+});
